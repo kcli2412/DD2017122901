1b2e4b8a684c60e100f9b14916b62221a206cc9b
kcli2412 <kcli2412@gmail.com>
2017-12-29 10:56:36 +0800

    分享圖片

--- app/src/main/java/com/example/student/db2017122901/MainActivity.java
+++ app/src/main/java/com/example/student/db2017122901/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.student.db2017122901;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +20,17 @@ public class MainActivity extends AppCompatActivity {
         it.setAction(Intent.ACTION_SEND);
         it.setType("text/plain");
         it.putExtra(Intent.EXTRA_TEXT,"這是要分享的文字123123");
+        startActivity(it);
+    }
+
+    public void click2(View v)
+    {
+        Intent it = new Intent();
+        it.setAction(Intent.ACTION_SEND);
+        it.setType("image/jpeg");
+        Uri uri = Uri.parse("android.resource://" +
+                getApplicationContext().getPackageName() + "/" + R.drawable.flower);
+        it.putExtra(Intent.EXTRA_STREAM, uri);
         startActivity(it);
     }
 }
