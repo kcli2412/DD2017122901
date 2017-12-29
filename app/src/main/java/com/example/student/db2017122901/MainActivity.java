4ab24f97c4e87e5301d34a0cade09592fdc0c668
kcli2412 <kcli2412@gmail.com>
2017-12-29 09:53:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/db2017122901/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.student.db2017122901;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v)
+    {
+        Intent it = new Intent();
+        it.setAction(Intent.ACTION_SEND);
+        it.setType("text/plain");
+        it.putExtra(Intent.EXTRA_TEXT,"這是要分享的文字");
+        startActivity(it);
+    }
+}
